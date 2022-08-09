caebb697fc9d059e581d3d4db542c87cbf93917b
marcio-breno <mbreno.dev@gmail.com>
2022-08-08 23:01:36 -0300

    primeiro commit

---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,18 @@
+export class NegociaoesView {
+    template() {
+        return `
+    <table class="table table-houver table-bordered">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Quantidade</th>
+          <th>Valor</th>
+        </tr>
+      </thead>
+      <tbody>
+      
+      </tbody>
+    </table>    
+    `;
+    }
+}
