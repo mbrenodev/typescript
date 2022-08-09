5bdadf175fd2843cf0c76ee66966e0696e8dea3b
marcio-breno <mbreno.dev@gmail.com>
2022-08-09 10:48:33 -0300

    Elaborando uma solução de view

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,5 +1,13 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociaoesView {
-  template(): string {
+  private elemento: HTMLElement;
+
+  constructor(seletor: string) {
+    this.elemento = document.querySelector(seletor);
+  }
+
+  template(model: Negociacoes): string {
     return `
     <table class="table table-houver table-bordered">
       <thead>
@@ -10,9 +18,23 @@ export class NegociaoesView {
         </tr>
       </thead>
       <tbody>
-      
+        ${model.lista().map(negociacao => {
+      return `
+            <tr>
+              <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+              <td> ${negociacao.quantidade} </td>
+              <td> ${negociacao.valor} </td>
+            </tr>
+          `;
+    }).join('')}
       </tbody>
     </table>    
     `;
+  }
+
+  update(model: Negociacoes): void {
+    const template = this.template(model);
+    console.log(template);
+    this.elemento.innerHTML = template;
   }
 }
